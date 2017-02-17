f9ff53bacaa510d3b63918b422e897aa71c3b7ac
Raven Ackerman <rayvayvo@gmail.com>
2017-02-17 16:50:31 +0000

    adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var raining = true;
+var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
