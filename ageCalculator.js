df9b7d6f9928666b2eded860a5d272ef220f6404
Raven Ackerman <rayvayvo@gmail.com>
2017-02-17 17:34:10 +0000

    adds looping exercises

---
+++ ageCalculator.js
@@ -0,0 +1,10 @@
+
+
+function ageCalculator(name, yearOfbirth, currentYear) {
+
+var age = currentYear - yearOfbirth;
+  return name + " is " + age + " years old."
+}
+
+console.log(ageCalculator('Miranda', 1983, 2015));
+console.log(ageCalculator('Ferdinand', 1988, 2015));
---
+++ which-school.js
@@ -0,0 +1,19 @@
+function whichSchool(age) {
+  // Your code in here ...
+
+  var school
+  if (age < 13)
+    school = 'Elementary School';
+  else if (age >= 13 && age < 18)
+    school = 'Secondary School';
+  else
+    school = 'Lighthouse Labs';
+  return school;
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
